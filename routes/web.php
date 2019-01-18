fca0ba6681212938e0fd9169d1edc24c61883bd8
JiroZhang <310704146@qq.com>
2019-01-18 11:48:37 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,9 @@
    Route::get('/','StaticPagesController@home')->name('home');
    Route::get('/help','StaticPagesController@help')->name('help');
    Route::get('/about','StaticPagesController@about')->name('about');
+
+   /*
+    * 登录注册模块路由
+    * */
+   Route::get('signup','UsersController@create')->name('signup');
+
